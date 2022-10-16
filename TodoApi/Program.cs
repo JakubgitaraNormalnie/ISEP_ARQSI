edb36608f94386b7dbe2b67da1cb6d9512074554
DanyKrk <daniel.k.8576@gmail.com>
2022-10-16 15:36:36 +0100

    Finish the web API with APS.NET core tutorial

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -13,14 +13,15 @@ builder.Services.AddDbContext<TodoContext>(opt =>
 //{
 //    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
 //});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     //app.UseSwagger();
     //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
